86c6d8ab9b04443c16f68f3609c20215f73cf9a6
MAbdullahLuqman <luqmanabdullah2005@gmail.com>
2026-08-18 00:18:02 +0500

    fixing vercel errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -8,6 +9,7 @@ export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tanstackStart({ server: { entry: "server" } }),
+    nitro(),
     react(),
     tailwindcss(),
   ],
